6c1a86bd18652d72d9f1514762bebba9f11f7e63
Jakub Kret <jakub.a.kret@gmail.com>
2017-12-03 19:56:05 +0100

    Implement Motor class

---
+++ motor/Motor.py
@@ -0,0 +1,56 @@
+import time
+import threading
+import RPi.GPIO as GPIO
+
+
+class Motor(threading.Thread):
+    def __init__(self, threadID, name, pins, motor_control):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.__control = motor_control
+        self.__sequence = [
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1]
+        ]
+        self.__pins = pins
+        self.__max_speed = 100.0
+        self.set_up_pins()
+
+    def set_up_pins(self):
+        GPIO.setmode(GPIO.BCM)
+        for pin in self.__pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, False)
+
+    def run(self):
+        step_count = len(self.__sequence)
+        step_counter = 0
+
+        while True:
+            print(self.name)
+            print(step_counter)
+            print(self.__sequence[step_counter])
+
+            for pin in range(0, 4):
+                xpin = self.__pins[pin]  # Get GPIO
+                if self.__sequence[step_counter][pin] != 0:
+                    print(" Enable GPIO %i" % xpin)
+                    GPIO.output(xpin, True)
+                else:
+                    GPIO.output(xpin, False)
+            print('-----------')
+
+            step_counter += self.__control.direction
+            if step_counter >= step_count:
+                step_counter = 0
+            if step_counter < 0:
+                step_counter = step_count
+
+            time.sleep(1 / (self.__max_speed * self.__control.speed))
