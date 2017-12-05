ffb8466d1eddc5a8e9dcd08c159c7cd466cb4db4
Jakub Kret <jakub.a.kret@gmail.com>
2017-12-05 18:40:12 +0100

    Add test implementation of BluetoothControl

--- control/BluetoothControl.py
+++ control/BluetoothControl.py
@@ -1,3 +1,39 @@
+import socket
+
+HOST_MAC_ADDRESS = 'b8:27:eb:16:7a:5c'
+PORT = 3
+BACKLOG = 1
+SIZE = 1024
+
+
 class BluetoothControl:
-    def __init__(self):
-        pass
+    def __init__(self, queue):
+        self.__command_queue = queue
+
+    def __push_command(self, command):
+        self.__command_queue.put(command)
+
+    def __parse_command(self, command):
+        split_command = command.split(';')
+        if len(split_command) == 2:
+            return {
+                'x': split_command[0],
+                'y': split_command[1]
+            }
+        return {}
+
+    def run(self):
+        s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        s.bind((HOST_MAC_ADDRESS, PORT))
+        s.listen(BACKLOG)
+
+        try:
+            client, address = s.accept()
+            while True:
+                data = client.recv(SIZE)
+                command = self.__parse_command(data)
+                print(command)
+        except:
+            print("Closing socket")
+            client.close()
+            s.close()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# import queue
-#
+import queue
+
 # from vehicle.Vehicle import Vehicle
 # from control.KeyboardControl import KeyboardControl
-#
-# command_queue = queue.Queue(50)
-#
+from control.BluetoothControl import BluetoothControl
+
+command_queue = queue.Queue(50)
+
 # vehicle = Vehicle(command_queue)
 # control = KeyboardControl(command_queue)
 #
@@ -12,28 +13,5 @@
 # control.run()
 # vehicle.join()
 
-"""
-A simple Python script to receive messages from a client over
-Bluetooth using Python sockets (with Python 3.3 or above).
-"""
-
-import socket
-
-hostMACAddress = 'b8:27:eb:16:7a:5c' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
-backlog = 1
-size = 1024
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
-s.listen(backlog)
-try:
-    client, address = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-            client.send(data)
-except:
-    print("Closing socket")
-    client.close()
-    s.close()
+control = BluetoothControl(command_queue)
+control.run()
